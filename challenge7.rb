d135101cb2635413157ba04d68ead0cbbc3909a4
Ross Hepburn <hepburnjrl@gmail.com>
2014-02-25 18:21:09 +0000

    Ex 4

--- challenge7.rb
+++ challenge7.rb
@@ -2,7 +2,7 @@ def file_input
 	File.open('./hello_from_ruby.txt', 'a') do |file|
 		puts "Input text"
 		user_input = gets.chomp.to_s
-		puts "Type 'save' to save"
+		puts "save/read"
 		save = gets.chomp.downcase.to_s
 
 		if save == "save"
@@ -12,4 +12,13 @@ def file_input
 	end
 end
 
+def read_file(save)
+	File.open('./hello_from_ruby.txt', 'r') do |file_read|
+		if save == "read"
+			puts file_read.read
+		end
+	end
+end
+
 save = file_input
+read_file(save)

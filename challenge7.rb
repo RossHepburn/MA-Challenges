2cf6f1c9d8833acadc8fb001d40ae5088561108b
Ross Hepburn <hepburnjrl@gmail.com>
2014-02-25 18:22:23 +0000

    Ex 5

--- challenge7.rb
+++ challenge7.rb
@@ -2,7 +2,7 @@ def file_input
 	File.open('./hello_from_ruby.txt', 'a') do |file|
 		puts "Input text"
 		user_input = gets.chomp.to_s
-		puts "save/read"
+		puts "save/read/introspect"
 		save = gets.chomp.downcase.to_s
 
 		if save == "save"
@@ -20,5 +20,16 @@ def read_file(save)
 	end
 end
 
+def print_file(save)
+	File.open('./challenge7.rb', 'r') do |file_read|
+		if save == "introspect"
+			puts file_read.read
+		end
+	end
+end
+
+
+
 save = file_input
 read_file(save)
+print_file(save)
